d11418b23ba33462dfb190921d2bf81842f6b434
Geoffrey Martin-Noble <gmngeoffrey@gmail.com>
2024-10-11 15:42:41 -0700

    Prepare for publishing to Firefox AMO
    Dropped version to 0.1, since this isn't really polished and thoroughly
    tested. Changed name to flow better. Adjustments for `web-ext build`.

---
+++ web-ext-config.mjs
@@ -0,0 +1,11 @@
+export default {
+    ignoreFiles: [
+        'package.json',
+        'package-lock.json',
+        'yarn.lock',
+        '*.code-workspace',
+        'bookmarklet',
+        '*.sh',
+        '*.mjs',
+    ],
+};
